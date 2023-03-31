804dbe40559a75284d5825b500ab03930389f95d
AlexanderUp <alexander.uperenk0@yandex.ru>
2023-03-31 17:02:21 +0300

    Migrated to poetry, nitpick, wemake-python-styleguide

--- link_extractor.py
+++ link_extractor.py
@@ -4,26 +4,26 @@ from typing import Union
 import requests
 
 
-def parse_json(data: dict) -> list:
+def parse_json(json_data: dict) -> list:
     link_list = []
-    obj_type: Union[str, None] = data.get("type")
-    if obj_type == "file":
-        download_url = data.get("download_url")
+    obj_type: Union[str, None] = json_data.get('type')
+    if obj_type == 'file':
+        download_url = json_data.get('download_url')
         link_list.append(download_url)
-    elif obj_type == "dir":
-        download_url = data.get("url")
-        link_list.extend(parse_dir(download_url))  # type:ignore
+    elif obj_type == 'dir':
+        download_url = json_data.get('url')
+        link_list.extend(parse_repo_dir(download_url))  # type:ignore
     else:
-        raise ValueError("Unknown object type.")
+        raise ValueError('Unknown object type.')
     return link_list
 
 
-def parse_dir(url: str) -> list:
+def parse_repo_dir(url: str) -> list:
     link_list = []
     response = requests.get(url, timeout=10)
-    json_obj = json.loads(response.text)
+    json_objs = json.loads(response.text)
 
-    for obj in json_obj:
-        link_list.extend(parse_json(obj))
+    for json_obj in json_objs:
+        link_list.extend(parse_json(json_obj))
 
     return link_list
--- main.py
+++ main.py
@@ -1,75 +1,73 @@
 import asyncio
-import concurrent.futures
 import logging
 import tempfile
+from concurrent.futures import ProcessPoolExecutor
 from pathlib import Path
 
-import aiohttp
 from aiofile import async_open
 from aiohttp import ClientSession
 
-from link_extractor import parse_dir
+from link_extractor import parse_repo_dir
 from utils import get_hash
 
 logging.basicConfig(
-    format="[%(levelname)s]:%(asctime)s - %(message)s",
-    datefmt="%m/%d/%Y %I:%M:%S %p",
+    format='[%(levelname)s]:%(asctime)s - %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S %p',
     level=logging.INFO,
 )
 
 
-async def download_file(url: str, session: ClientSession) -> bytes:
-    logging.info(f"Processing: {url}")
-    async with session.get(url) as response:
+async def download_file(file_url: str, session: ClientSession) -> bytes:
+    logging.info(f'Processing: {file_url}')
+    async with session.get(file_url) as response:
         return await response.read()
 
 
-async def write_file_to_tempdir(url: str, data: bytes, directory: str) -> None:
-    path_to_file = Path(directory) / Path(url).name
-    async with async_open(path_to_file, "bw") as file:
-        await file.write(data)
+async def write_file_to_tempdir(file_url: str, file_content: bytes, directory: str) -> None:
+    path_to_file = Path(directory) / Path(file_url).name
+    async with async_open(path_to_file, 'bw') as out_file:
+        await out_file.write(file_content)
 
 
 async def process_file(url: str,
                        session: ClientSession,
                        directory: str,
-                       semaphore: asyncio.Semaphore) -> None:
+                       semaphore: asyncio.Semaphore
+                       ) -> None:
     async with semaphore:
         file_content = await download_file(url, session)
         await write_file_to_tempdir(url, file_content, directory)
 
 
-async def set_tasks(urls: list,
+async def run_tasks(urls: list,
                     directory: str,
                     concur_task_num: int) -> None:
-    session = aiohttp.ClientSession()
+    session = ClientSession()
     sem = asyncio.Semaphore(concur_task_num)
     tasks = [asyncio.create_task(
         process_file(url, session, directory, sem)) for url in urls]
-    logging.info(f"{len(tasks)} urls collected.")
+    logging.info(f'{len(tasks)} urls collected.')
     await asyncio.gather(*tasks)
     await session.close()
 
 
-async def main(url, concur_task_num=3):
-    logging.info("Script started.")
-    urls = parse_dir(url)
+def main(url, concur_task_num=3):
+    logging.info('Script started.')
+    urls = parse_repo_dir(url)
 
     with tempfile.TemporaryDirectory() as tempdir:
-        logging.info(f"Temporary dir: {tempdir}")
-
-        await set_tasks(urls, tempdir, concur_task_num)
+        asyncio.run(run_tasks(urls, tempdir, concur_task_num))
 
         paths = list(Path(tempdir).iterdir())
 
-        with concurrent.futures.ProcessPoolExecutor() as executor:
-            for path, hash in zip(paths, executor.map(get_hash, paths)):
-                print(f"{hash} {path}")
+        with ProcessPoolExecutor() as executor:
+            for path, calculated_hash in zip(paths, executor.map(get_hash, paths)):
+                print(f'{calculated_hash} {path}')
 
-    logging.info("Script completed.")
+    logging.info('Script completed.')
 
 
-if __name__ == "__main__":
-    url = "https://gitea.radium.group/api/v1/repos/radium/project-configuration/contents"  # noqa
+if __name__ == '__main__':
+    url = 'https://gitea.radium.group/api/v1/repos/radium/project-configuration/contents'  # noqa
 
-    asyncio.run(main(url))
+    main(url)
