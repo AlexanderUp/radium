5fe46ca2d8e5f901b79da426d2506bb622436f2b
AlexanderUp <alexander.uperenk0@yandex.ru>
2023-03-31 23:58:06 +0300

    Refactored as per pep8

--- aux_utils.py
+++ aux_utils.py
@@ -8,6 +8,8 @@ def get_hash(path_to_file: str, chunk_size: int = CHUNK_SIZE) -> str:
     """Calculate hash of file correspond to given filepath."""
     with open(path_to_file, 'br') as in_file:
         hasher = hashlib.sha256()
-        while chunk := in_file.read(chunk_size):
+        chunk = in_file.read(chunk_size)
+        while chunk:
             hasher.update(chunk)
+            chunk = in_file.read(chunk_size)
     return hasher.hexdigest()
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from concurrent.futures import ProcessPoolExecutor
 from pathlib import Path
 
 from aiofile import async_open
-from aiohttp import ClientSession
+from aiohttp import ClientError, ClientSession
 
 from aux_utils import get_hash
 from link_extractor import parse_repo_dir
@@ -15,27 +15,35 @@ from loggers import logger
 
 async def download_file(file_url: str, session: ClientSession) -> bytes:
     """Download file with given URL."""
-    logger.info(f'Processing: {file_url}')
-    async with session.get(file_url) as response:
-        return await response.read()
+    logger.info('Processing: {0}'.format(file_url))
+    file_content = b''
+    try:
+        async with session.get(file_url) as response:
+            file_content = await response.read()
+    except ClientError as err:
+        logger.error(err)
+    return file_content
 
 
 async def write_file_to_tempdir(
-        file_url: str,
-        file_content: bytes,
-        directory: str,
+    file_url: str,
+    file_content: bytes,
+    directory: str,
 ) -> None:
     """Write file to given directory."""
     path_to_file = Path(directory) / Path(file_url).name
-    async with async_open(path_to_file, 'bw') as out_file:
-        await out_file.write(file_content)
+    try:
+        async with async_open(path_to_file, 'bw') as out_file:
+            await out_file.write(file_content)
+    except OSError as err:
+        logger.error(err)
 
 
 async def process_file(
-        url: str,
-        session: ClientSession,
-        directory: str,
-        semaphore: asyncio.Semaphore,
+    url: str,
+    session: ClientSession,
+    directory: str,
+    semaphore: asyncio.Semaphore,
 ) -> None:
     """Download file from given URL and write them to specified directory."""
     async with semaphore:
@@ -44,17 +52,25 @@ async def process_file(
 
 
 async def run_tasks(
-        urls: list,
-        directory: str,
-        session: ClientSession,
-        concur_task_num: int,
+    urls: list,
+    directory: str,
+    session: ClientSession,
+    concur_task_num: int,
 ) -> None:
     """Create and schedule tasks for execution."""
     sem = asyncio.Semaphore(concur_task_num)
     tasks = [process_file(url, session, directory, sem) for url in urls]
-    logger.info(f'{len(tasks)} urls collected.')
+    logger.info('{0} urls collected.'.format(len(tasks)))
     await asyncio.gather(*tasks)
     await session.close()
+
+
+def log_file_hashes(paths: list) -> None:
+    """Get file hashes with ProcessPoolExecutor."""
+    with ProcessPoolExecutor() as executor:
+        zipped_paths = zip(paths, executor.map(get_hash, paths))
+        for path, calculated_hash in zipped_paths:
+            logger.info('{0} {1}'.format(calculated_hash, path))
 
 
 async def main(url, concur_task_num=3):
@@ -68,11 +84,7 @@ async def main(url, concur_task_num=3):
             await run_tasks(urls, tempdir, session, concur_task_num)
 
             paths = list(Path(tempdir).iterdir())
-
-            with ProcessPoolExecutor() as executor:
-                zipped_paths = zip(paths, executor.map(get_hash, paths))
-                for path, calculated_hash in zipped_paths:
-                    logger.info(f'{calculated_hash} {path}')
+            log_file_hashes(paths)
 
     logger.info('Script completed.')
 
